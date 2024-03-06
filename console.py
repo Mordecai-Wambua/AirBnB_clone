759b9b83dcd8bcd0c782a6cd45f015f75704e82c
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-03-06 18:34:12 +0300

    trial command interpreter

---
+++ console.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Actual python terminal."""
+
+
+import cmd
+class Shell(cmd.Cmd):
+    """The entry point of the command interpreter."""
+
+    prompt = "(hbnb) "
+    doc_header = "Documented commands (type help <topic>):"
+    ruler = '='
+
+    def do_quit(self, line):
+        """Command to exit cleanly."""
+        return True
+
+    def emptyline(self):
+        """Define action performed incase of empty line input."""
+        pass
+
+    def do_EOF(self, line):
+        """Allow the program to exit cleanly."""
+        return True
+
+    def postloop(self):
+        """Acts when cmdloop is about to return."""
+        print()
+
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) > 1:
+        Shell().onecmd(' '.join(sys.argv[1:]))
+    else:
+        Shell().cmdloop()
